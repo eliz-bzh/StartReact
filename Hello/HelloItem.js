82c8d9c9c9a0e6dd15c1d316fca81e81414422fa
Liz <eliz.com>
2019-10-24 01:52:04 +0300

    Hello

---
+++ Hello/HelloItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HelloItem extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return <h1>Hello</h1>
+    }
+
+}
+export default HelloItem;
