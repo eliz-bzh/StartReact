14f263c97534b4c33e6e4ab0989d18d9e3868ae9
Liz <eliz.com>
2019-10-24 01:55:17 +0300

    TodoList Start

---
+++ TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TodoList() {
+    return(
+        <ul>
+            <li>TodoList</li>
+            <li>TodoList</li>
+            <li>TodoList</li>
+        </ul>
+    );
+}
